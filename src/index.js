6e01a1375264703c0e181fa147b4f0c8ecafee56
pxperfectmike <pxperfectmike@gmail.com>
2023-05-28 15:43:24 -0700

    add second iteration of initial structure to the main function

--- src/index.js
+++ src/index.js
@@ -14,8 +14,20 @@ async function main() {
         const data = await fs.readFile('./config.json', 'utf-8');
         const config = JSON.parse(data);
 
+        // Interpret the configuration
+        const interpretedConfig = interpretConfig(config);
+
         // Generate tests based on the configuration
-        await generateTests(config);
+        const testCode = generateTests(interpretedConfig);
+
+        // Save the test code to a file
+        await fs.writeFile('./generatedTests.js', testCode);
+
+        // Run the tests
+        const results = runTests('./generatedTests.js');
+
+        // Print the results
+        console.log(results);
     } catch (err) {
         console.error('An error occurred:', err);
     }
